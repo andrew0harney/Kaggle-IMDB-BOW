93bf2683b66bb2a05ebd00df2c012fa440cea715
Andrew O'Harney <andy@singletrack.com>
2015-06-24 16:35:49 +0100

    Update semantic lda predictions

--- random_forrest_lda_classifier.py
+++ random_forrest_lda_classifier.py
@@ -13,9 +13,15 @@ from bs4 import BeautifulSoup
 import string
 import numpy as np
 from gensim import corpora, models
-import pprint
 from sklearn.externals import joblib
 import logging
+
+#Config
+num_lda_topics = 2
+num_forest_estimators = 150
+num_lda_passes = 100
+train_set_percent = 80
+
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -32,7 +38,7 @@ def review_to_words(review):
 
 
 def convert_lda_output(prediction):
-    output = [0,0]
+    output = [0] * num_lda_topics
     for i,x in prediction:
         output[i] = x
     return output
@@ -45,9 +51,11 @@ log.info('Loading train/test set')
 train = pd.read_csv(os.path.join(os.getcwd(), 'data', 'labeledTrainData.tsv'), header=0, \
                 delimiter="\t", quoting=3)
 train = train.reindex(np.random.permutation(len(train))).reset_index()
-test = train[20000:]
-train = train[:20000]
+test = train[train_set_percent*len(train)/100:]
+train = train[:train_set_percent*len(train)/100]
 
+log.info('Training set %d' % (len(train)))
+log.info('Testing set %d' % (len(test)))
 
 vectoriser = CountVectorizer(analyzer = "word",
                             tokenizer = None,
@@ -56,22 +64,28 @@ vectoriser = CountVectorizer(analyzer = "word",
                              max_features = 5000)
 
 
+semanticLDAVectoriser = CountVectorizer(analyzer = "word",
+                            tokenizer = None,
+                             preprocessor = None,
+                             stop_words = None,
+                             max_features = 5000)
+
 #Train LDA for positive and negative topics
 log.info('Cleaning training data')
 clean_train_reviews = [review for review in train['review'].values.tolist()]
 reviews = [" ".join(review_to_words(review)) for review in clean_train_reviews]
 train_data_features = vectoriser.fit_transform(reviews).toarray()
 
-vocab = corpora.Dictionary([vectoriser.get_feature_names()])
-tokenised_train = [vocab.doc2bow(get_semantics(review)) for review in reviews]
+semanticLDAVectoriser.fit_transform([" ".join(get_semantics(review)) for review in reviews])
+vocab = corpora.Dictionary([semanticLDAVectoriser.get_feature_names()])
+lda_tokenised_train = [vocab.doc2bow(get_semantics(review)) for review in reviews]
 
 log.info('Training LDA')
-lda = models.ldamodel.LdaModel(corpus=tokenised_train, id2word=vocab, num_topics=2, passes=100)
+lda = models.ldamodel.LdaModel(corpus=lda_tokenised_train, id2word=vocab, num_topics=num_lda_topics, passes=num_lda_passes)
 lda.save(fname=os.path.join(os.getcwd(), 'models', 'lda.model'))
 
-
 log.info('Adding LDA features to training')
-train_lda_out = lda[tokenised_train]
+train_lda_out = lda[lda_tokenised_train]
 train_topics = np.array([convert_lda_output(prediction) for prediction in train_lda_out])
 train_data_features = np.hstack((train_data_features, train_topics))
 
@@ -84,15 +98,15 @@ joblib.dump(forest, os.path.join(os.getcwd(), 'models', 'forest.model'))
 #Test
 log.info('Cleaning test data')
 clean_test_reviews = [" ".join(review_to_words(review)) for review in test['review'].values.tolist()]
-tokenised_test = [vocab.doc2bow(get_semantics(review)) for review in clean_test_reviews]
+lda_tokenised_test = [vocab.doc2bow(get_semantics(review)) for review in clean_test_reviews]
 
 log.info('Adding LDA features to testing')
-test_lda_out = lda[tokenised_test]
+test_lda_out = lda[lda_tokenised_test]
 test_topics = np.array([convert_lda_output(prediction) for prediction in test_lda_out])
 test_data_features = np.hstack((vectoriser.transform(clean_test_reviews).toarray(), test_topics))
 
 log.info('Predicting')
-result = forest.predict_proba(test_data_features)
+result = forest.predict(test_data_features)
 
 output = pd.DataFrame( data={"id":test["id"], "sentiment":result, "actual":test["sentiment"]} )
 output.to_csv(os.path.join(os.getcwd(), 'results', 'bowRandomForestResults.csv'), index=False, quoting=3)
